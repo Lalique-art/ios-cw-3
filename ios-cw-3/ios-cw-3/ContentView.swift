61b550f4b244b27e2639e0d31822c5676edf0e7e
maryam matiri <64448234+Lalique-art@users.noreply.github.com>
2022-03-07 13:21:15 +0300

    done

--- ios-cw-3/ios-cw-3/ContentView.swift
+++ ios-cw-3/ios-cw-3/ContentView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    var body: some View{
+    
+        VStack("my fav")
+        
     }
-}
+    image{""}
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
